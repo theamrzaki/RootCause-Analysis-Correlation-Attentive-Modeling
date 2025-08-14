979c5b9701a8446da0a145931cf35fa0b3312f80
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2025-08-14 14:43:32 -0400

    (msds with GNN and attention)

--- correlated_KL.sh
+++ correlated_KL.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 seeds=(4)
-attention_modes=("none")
+coeff_architecture=(deep_mlp gnn_attention)
+dataset="msds"
 
-for correlated_KL in 1; do
-  for attention_mode in "${attention_modes[@]}"; do
+for correlated_KL in 0 1; do
+  for arch in "${coeff_architecture[@]}"; do
     for seed in "${seeds[@]}"; do
-      echo "Running with correlated_KL=$correlated_KL, seed=$seed, global_attention_over_all_lag=$attention_mode"
+      echo "Running for $dataset on seed=$seed with (correlated_KL=$correlated_KL,  architecture=$arch)"
       python3 main.py \
         --correlated_KL="$correlated_KL" \
         --seed="$seed" \
-        --dataset="swat" \
-        --lambda_indep=0.5 \
-        --lambda_corr=1.0 \
-        --global_attention_over_all_lag="$attention_mode"
+        --dataset="$dataset" \
+        --coeff_architecture="$arch"
     done
   done
 done
