e48ef4e3078bedf24c3e7c18c52790ed1854d25f
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2025-08-18 19:02:11 -0400

    (script)

--- correlated_KL.sh
+++ correlated_KL.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-seeds=(5)  
-coeff_architecture=(TemporalGNN_Attention)
+seeds=(1 2 3 4 11 12 13)
+coeff_architecture=(deep_mlp TemporalGNN_Attention)
 dataset="msds"
 
-for correlated_KL in 0; do
+for correlated_KL in 0 1; do
   for arch in "${coeff_architecture[@]}"; do
     for seed in "${seeds[@]}"; do
-      echo "Running for $dataset on seed=$seed with (correlated_KL=$correlated_KL,  architecture=$arch)"
+      # set lr based on arch
+      if [ "$arch" == "deep_mlp" ]; then
+        lr="1e-6"
+      else
+        lr="1e-4"
+      fi
+
+      echo "Running for $dataset on seed=$seed with (correlated_KL=$correlated_KL, architecture=$arch, lr=$lr)"
       python3 main.py \
         --correlated_KL="$correlated_KL" \
         --seed="$seed" \
         --dataset="$dataset" \
-        --coeff_architecture="$arch"
+        --coeff_architecture="$arch" \
+        --lr="$lr"
     done
   done
 done
