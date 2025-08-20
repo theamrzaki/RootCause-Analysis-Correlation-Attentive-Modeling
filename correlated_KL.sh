2b9942387c62c7101ba860083b7172bafe56d8d6
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2025-08-20 10:37:33 -0400

    (script)

--- correlated_KL.sh
+++ correlated_KL.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-seeds=(13 12 11 4 3 2)
+seeds=(2 3 4 11 12 13)
 coeff_architecture=(TemporalGNN_Attention)
 dataset="msds"
-lrs=("5e-4")   # two different learning rates
-attention_dims=(128)  # different attention dimensions
-num_heads=(2)
-window_size_list=(1)  # different window sizes
-for window_size in "${window_size_list[@]}"; do
+lrs=("5e-4" "1e-4")   # two different learning rates
+attention_dims=(128 32 64 256)  # different attention dimensions
+num_heads=(2 4)
+
+for window_size in 1; do
   for arch in "${coeff_architecture[@]}"; do
     for seed in "${seeds[@]}"; do
       for lr in "${lrs[@]}"; do
         for att_dim in "${attention_dims[@]}"; do
           for heads in "${num_heads[@]}"; do
-            echo "Running for $dataset | seed=$seed | arch=$arch | lr=$lr | att_dim=$att_dim | heads=$heads | window=$window_size"
+            echo "Running for $dataset | seed=$seed | arch=$arch | lr=$lr | att_dim=$att_dim | heads=$heads"
 
             python3 main.py \
               --correlated_KL=0 \
@@ -23,7 +23,7 @@ for window_size in "${window_size_list[@]}"; do
               --lr="$lr" \
               --window_size="$window_size" \
               --attention_dim="$att_dim" \
-              --num_attention_heads="$heads" 
+              --num_attention_heads="$heads"
           done
         done
       done
