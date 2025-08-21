68813f0cea35007987981380e074455a3b423008
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2025-08-21 15:45:58 -0400

    (for deep_ml)

--- correlated_KL.sh
+++ correlated_KL.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-seeds=(2 3 4 11 12 13)
-coeff_architecture=(TemporalGNN_Attention)
-dataset="msds"
-lrs=("5e-4" "1e-4")   # two different learning rates
-attention_dims=(128 32 64 256)  # different attention dimensions
-num_heads=(2 4)
+seeds=(3 4 5 6 7 8 9 10)  # different seeds for reproducibility
+coeff_architecture=(deep_mlp)
+dataset="swat"
+lrs=("1e-6")   # two different learning rates
+attention_dims=(128)  # different attention dimensions
+num_heads=(2)
+corelated_list=(0)  # whether to use correlated KL or not
 
-for window_size in 1; do
-  for arch in "${coeff_architecture[@]}"; do
-    for seed in "${seeds[@]}"; do
-      for lr in "${lrs[@]}"; do
-        for att_dim in "${attention_dims[@]}"; do
-          for heads in "${num_heads[@]}"; do
-            echo "Running for $dataset | seed=$seed | arch=$arch | lr=$lr | att_dim=$att_dim | heads=$heads"
+for correlated_KL in "${corelated_list[@]}"; do
+  for window_size in 1; do
+    for arch in "${coeff_architecture[@]}"; do
+      for seed in "${seeds[@]}"; do
+        for lr in "${lrs[@]}"; do
+          for att_dim in "${attention_dims[@]}"; do
+            for heads in "${num_heads[@]}"; do
+              echo "Running for $dataset | seed=$seed | arch=$arch | lr=$lr | att_dim=$att_dim | heads=$heads"
 
-            python3 main.py \
-              --correlated_KL=0 \
-              --seed="$seed" \
-              --dataset="$dataset" \
-              --coeff_architecture="$arch" \
-              --lr="$lr" \
-              --window_size="$window_size" \
-              --attention_dim="$att_dim" \
-              --num_attention_heads="$heads"
+              python3 main.py \
+                --correlated_KL="$correlated_KL" \
+                --seed="$seed" \
+                --dataset="$dataset" \
+                --coeff_architecture="$arch" \
+                --lr="$lr" \
+                --window_size="$window_size" \
+                --attention_dim="$att_dim" \
+                --num_attention_heads="$heads" \
+                --early_stopping=1 \
+                --epochs=5000 
+            done
           done
         done
       done
