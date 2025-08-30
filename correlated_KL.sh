29ec1164569d886a36813f0ce4db59c23520cc75
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2025-08-30 11:23:44 -0400

    (with msds), (losses dont make better results)

--- correlated_KL.sh
+++ correlated_KL.sh
@@ -2,21 +2,21 @@
 
 # --- Configurations ---
 seeds=(1 2 3)
-coeff_architecture=("deep_mlp")
+coeff_architecture=("TemporalGNN_Attention")
 dataset=("swat")
-lrs=("1e-6")
-attention_dims=(128)
-num_heads=(2)
+lrs=("1e-4" "1e-5" "5e-5")
+attention_dims=(64 128 256)
+num_heads=(1 2 4)
 corelated_list=(0)
-window_size=(2 3 4)
+window_size=(1)
 
 # --- Helper function to run experiments ---
 run_experiment1() {
     local use_amoc=$1  # 0 = no AMOC, 1 = AMOC
     for window_size_item in "${window_size[@]}"; do
         for arch in "${coeff_architecture[@]}"; do
-            for seed in "${seeds[@]}"; do
-                for lr in "${lrs[@]}"; do
+            for lr in "${lrs[@]}"; do
+                for seed in "${seeds[@]}"; do
                     for att_dim in "${attention_dims[@]}"; do
                         for heads in "${num_heads[@]}"; do
                             echo "Running: dataset=$dataset | seed=$seed | arch=$arch | lr=$lr | att_dim=$att_dim | heads=$heads | AMOC=$use_amoc"
@@ -29,11 +29,9 @@ run_experiment1() {
                                 --lr=$lr \
                                 --window_size=$window_size_item \
                                 --attention_dim=$att_dim \
-                                --num_attention_heads=$heads \
-                                --early_stopping=1 \
-                                --epochs=5000"
+                                --num_attention_heads=$heads"
                             if [ "$use_amoc" -eq 1 ]; then
-                                cmd="$cmd --AMOC_Loss=1 --mean_std_recon_loss=1"
+                                cmd="$cmd --AMOC_Loss=0 --mean_std_recon_loss=0"
                             fi
 
                             eval $cmd
@@ -50,104 +48,3 @@ run_experiment1() {
 run_experiment1 1
 
 
-
-
-
-seeds=(1 2 3)
-coeff_architecture=("TemporalGNN_Attention")
-dataset=("msds")
-lrs=("5e-4")
-attention_dims=(128)
-num_heads=(2)
-corelated_list=(0)
-window_size=(2 3 4)
-
-# --- Helper function to run experiments ---
-run_experiment2() {
-    local use_amoc=$1  # 0 = no AMOC, 1 = AMOC
-    for window_size_item in "${window_size[@]}"; do
-        for arch in "${coeff_architecture[@]}"; do
-            for seed in "${seeds[@]}"; do
-                for lr in "${lrs[@]}"; do
-                    for att_dim in "${attention_dims[@]}"; do
-                        for heads in "${num_heads[@]}"; do
-                            echo "Running: dataset=$dataset | seed=$seed | arch=$arch | lr=$lr | att_dim=$att_dim | heads=$heads | AMOC=$use_amoc"
-
-                            cmd="python3 main.py \
-                                --correlated_KL=0 \
-                                --seed=$seed \
-                                --dataset=$dataset \
-                                --coeff_architecture=$arch \
-                                --lr=$lr \
-                                --window_size=$window_size_item \
-                                --attention_dim=$att_dim \
-                                --num_attention_heads=$heads"
-
-                            if [ "$use_amoc" -eq 1 ]; then
-                                cmd="$cmd --AMOC_Loss=1 --mean_std_recon_loss=1"
-                            fi
-
-                            eval $cmd
-                        done
-                    done
-                done
-            done
-        done
-    done
-}
-
-# --- Run experiments ---
-# 2. With AMOC
-run_experiment2 1
-
-
-
-
-# --- Configurations ---
-seeds=(1 2 3)
-coeff_architecture=("deep_mlp")
-dataset=("msds")
-lrs=("1e-6")
-attention_dims=(128)
-num_heads=(2)
-corelated_list=(0)
-window_size=(2 3 4)
-
-# --- Helper function to run experiments ---
-run_experiment3() {
-    local use_amoc=$1  # 0 = no AMOC, 1 = AMOC
-    for window_size_item in "${window_size[@]}"; do
-        for arch in "${coeff_architecture[@]}"; do
-            for seed in "${seeds[@]}"; do
-                for lr in "${lrs[@]}"; do
-                    for att_dim in "${attention_dims[@]}"; do
-                        for heads in "${num_heads[@]}"; do
-                            echo "Running: dataset=$dataset | seed=$seed | arch=$arch | lr=$lr | att_dim=$att_dim | heads=$heads | AMOC=$use_amoc"
-
-                            cmd="python3 main.py \
-                                --correlated_KL=0 \
-                                --seed=$seed \
-                                --dataset=$dataset \
-                                --coeff_architecture=$arch \
-                                --lr=$lr \
-                                --window_size=$window_size_item \
-                                --attention_dim=$att_dim \
-                                --num_attention_heads=$heads \
-                                --early_stopping=1 \
-                                --epochs=5000"
-                            if [ "$use_amoc" -eq 1 ]; then
-                                cmd="$cmd --AMOC_Loss=1 --mean_std_recon_loss=1"
-                            fi
-
-                            eval $cmd
-                        done
-                    done
-                done
-            done
-        done
-    done
-}
-
-# --- Run experiments ---
-# 2. With AMOC
-run_experiment3 1
