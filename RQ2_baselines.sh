d4c4597e1037d4015fd067f0465e78e5243c279e
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2025-09-02 15:17:11 -0400

    (baselines AERCA)

--- RQ2_baselines.sh
+++ RQ2_baselines.sh
@@ -43,4 +43,59 @@ run_experiment1() {
 }
 # --- Run experiments ---
 # 2. With AMOC
-run_experiment1 1
+#run_experiment1 1
+
+
+
+
+
+# --- Configurations ---
+seeds=(1 2 3)
+coeff_architecture=("deep_mlp")
+dataset=("swat" "msds")
+lrs=("1e-6")
+att_dim=256
+heads=2
+corelated_list=(0)
+window_size=(1)
+outer_heads=(4)
+outer_hidden_dim=(256)
+
+# --- Helper function to run experiments ---
+run_experiment_deepmlp() {
+    local use_amoc=$1  # 0 = no AMOC, 1 = AMOC
+    for window_size_item in "${window_size[@]}"; do
+        for data in "${dataset[@]}"; do
+            for arch in "${coeff_architecture[@]}"; do
+                for lr in "${lrs[@]}"; do
+                    for seed in "${seeds[@]}"; do
+                        for outer_att_dim_val in "${outer_hidden_dim[@]}"; do
+                            for outer_heads_val in "${outer_heads[@]}"; do
+                                echo "Running: dataset=$dataset | seed=$seed | arch=$arch | window_size=$window_size_item | lr=$lr"
+
+                                cmd="python3 main.py \
+                                    --correlated_KL=0 --mean_std_recon_loss=0 --AMOC_Loss=0 \
+                                    --encoder_alpha=0.5 --decoder_alpha=0.5 --encoder_gamma=0.5 --decoder_gamma=0.5 \
+                                    --encoder_lambda=0.5 --decoder_lambda=0.5 --beta=0.5 \
+                                    --lr=$lr \
+                                    --seed=$seed \
+                                    --dataset=$data \
+                                    --coeff_architecture=$arch \
+                                    --window_size=$window_size_item \
+                                    --training_aerca=1 \
+                                    --epochs=5000 \
+                                    --early_stopping=1 \
+                                    --results_csv=results_RQ2_baselines.csv"
+
+                                eval $cmd
+                            done
+                        done
+                    done
+                done
+            done
+        done
+    done
+}
+# --- Run experiments ---
+# 2. With AMOC
+run_experiment_deepmlp 1
