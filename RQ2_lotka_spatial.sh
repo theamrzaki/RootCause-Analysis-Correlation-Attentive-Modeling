389c6a84eff2956c7bba10992e8aef105a6b9e42
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2025-09-18 16:05:09 -0400

    (lotka)

---
+++ RQ2_lotka_spatial.sh
@@ -0,0 +1,71 @@
+# --- Configurations ---
+seeds=(1 2 3)
+coeff_architecture=("deep_mlp" "TemporalGNN_Attention" "TemporalGNN_Attention_fourier" "TemporalGNN_Attention_crossattn")
+dataset=("lotka_volterra")
+lrs=("1e-4")
+att_dim=32
+heads=2
+corelated_list=(0)
+window_size=(7)
+outer_heads=(2)
+outer_hidden_dim=(32)
+num_vars=(20 30 40 50 60)
+
+# --- Helper function to run experiments ---
+run_experiment_CrGSTA_spatial() {
+    local use_amoc=$1  # 0 = no AMOC, 1 = AMOC
+    for var in "${num_vars[@]}"; do  
+        for window_size_item in "${window_size[@]}"; do
+            for data in "${dataset[@]}"; do
+                for arch in "${coeff_architecture[@]}"; do
+                    for lr in "${lrs[@]}"; do
+                        for seed in "${seeds[@]}"; do
+                            for outer_att_dim_val in "${outer_hidden_dim[@]}"; do
+                                for outer_heads_val in "${outer_heads[@]}"; do
+                                    echo "Running: dataset=$dataset | seed=$seed | arch=$arch | window_size=$window_size_item | lr=$lr"
+                                    # only first time make preprocessing = 1 seed=1 and coeff_architecture="deep_mlp" and window_size=1
+                                    if [ "$arch" == "deep_mlp" ]; then
+                                        preprocessing_data=1
+                                    else
+                                        preprocessing_data=0
+                                    fi
+                                    echo "preprocessing_data=$preprocessing_data"      
+
+                                    cmd="python3 main.py \
+                                                    --correlated_KL=0 --mean_std_recon_loss=0 --AMOC_Loss=0 \
+                                        --encoder_alpha=0.5 --decoder_alpha=0.5 --encoder_gamma=0.5 --decoder_gamma=0.5 \
+                                        --encoder_lambda=0.5 --decoder_lambda=0.5 --beta=0.5 \
+                                        --lr=$lr \
+                                        --preprocessing_data=$preprocessing_data \
+                                        --seed=$seed \
+                                        --dataset="$data"\
+                                        --coeff_architecture=$arch \
+                                        --window_size=$window_size_item \
+                                        --training_aerca=1 \
+                                        --epochs=100 \
+                                        --early_stopping=0 \
+                                        --attention_dim=$att_dim \
+                                        --num_attention_heads=$heads \
+                                        --outer_heads_num=$outer_heads_val \
+                                        --outer_hidden_dim=$outer_att_dim_val \
+                                        --num_vars="$var" \
+                                        --results_csv=results_lorenz_spatial.csv"
+
+                                    eval $cmd
+                                done
+                            done
+                        done
+                    done
+                done
+            done
+        done
+    done
+}
+# --- Run experiments ---
+# 2. With AMOC
+run_experiment_CrGSTA_spatial 1
+
+
+
+
+
