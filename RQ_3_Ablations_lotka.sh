f445531225393134563ba2b2b9044e3552f90b99
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2025-09-22 05:31:14 -0400

    (RQ 3 lotka 64 dims)

--- RQ_3_Ablations_lotka.sh
+++ RQ_3_Ablations_lotka.sh
@@ -1,22 +1,23 @@
 # --- Configurations ---
-seeds=(1 2 3 4 5 6)
+seeds=(1 2)
 
 # Architectures
 main_architecture="TemporalGNN_Attention_fourier"
 baseline_architecture="TemporalGNN_Attention"
+main_model=("aerca_based")
 
 # Common settings
 dataset="lotka_volterra"
 lr="1e-4"
-att_dim=32
+att_dim=64
 heads=2
 window_sizes=(7)
 outer_heads=2
-outer_hidden_dim=32
+outer_hidden_dim=64
 
 # Ablation settings
-time_freq_representations=("normal")
-combine_methods=("freq_only")
+time_freq_representations=("normal" "mag_phase")
+combine_methods=("freq_only" "gated" "sum" "concat")
 
 # --- Helper function to run a single experiment ---
 run_single() {
@@ -28,12 +29,12 @@ run_single() {
 
     echo "Running: arch=$arch | dataset=$dataset | seed=$seed | ws=$ws | tf=$tf | combine=$combine"
 
-    # Preprocessing only for first time TemporalGNN_Attention
-    #if [ "$arch" == "TemporalGNN_Attention" ]; then
-    #    preprocessing_data=1
-    #else
-    #    preprocessing_data=0
-    #fi
+     Preprocessing only for first time TemporalGNN_Attention
+    if [ "$arch" == "TemporalGNN_Attention" ]; then
+        preprocessing_data=1
+    else
+        preprocessing_data=0
+    fi
     
     python3 main.py \
         --correlated_KL=0 --mean_std_recon_loss=0 --AMOC_Loss=0 \
@@ -41,6 +42,7 @@ run_single() {
         --encoder_lambda=0.5 --decoder_lambda=0.5 --beta=0.5 \
         --coeff_architecture="$arch" \
         --preprocessing_data=0 \
+        --main_model=$main_model \
         ${tf:+--time_freq_representation="$tf"} \
         ${combine:+--combine_method="$combine"} \
         --lr="$lr" \
@@ -55,7 +57,7 @@ run_single() {
         --outer_heads_num="$outer_heads" \
         --outer_hidden_dim="$outer_hidden_dim" \
         --num_vars=40 \
-        --results_csv=RQ_3_results_all_attention_lotka.csv
+        --results_csv=RQ_3_lotka.csv
 }
 
 # --- Run experiments ---
@@ -63,7 +65,7 @@ for seed in "${seeds[@]}"; do
     for ws in "${window_sizes[@]}"; do
         
         # --- Baseline (no ablations, no tf/combine flags) ---
-        #run_single "$baseline_architecture" "$seed" "$ws"
+        run_single "$baseline_architecture" "$seed" "$ws"
 
         # --- Main architecture with ablations ---
         for tf in "${time_freq_representations[@]}"; do
