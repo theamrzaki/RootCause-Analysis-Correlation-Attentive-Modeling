5d4bf0b650d2557e93b2e5f0ab41acc98c16f4d4
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2025-09-22 20:42:33 -0400

    (iTransformer)

--- RQ_1.sh
+++ RQ_1.sh
@@ -41,7 +41,46 @@ run_experiment_RoGSTA_SWAT() {
 
 
 
+# --- Configurations ---
+seeds=(1 2 3 4 5 6)
+dataset=("swat")
+lrs=("1e-4")
+window_size=(5 1 7 10 12)
+main_model=("iTransformer")
+attention_dim=256
+heads=2
+# --- Helper function to run experiments ---
+run_experiment_RoGSTA_SWAT_iTransformer() {
+    for seed in "${seeds[@]}"; do
+        for window_size_item in "${window_size[@]}"; do
+                        for main_model_item in "${main_model[@]}"; do
+                                echo "Running: dataset=$dataset | seed=$seed | window_size=$window_size_item | lr=$lrs | main_model=$main_model_item"
 
+                                cmd="python3 main.py \
+                                                --correlated_KL=0 --mean_std_recon_loss=0 --AMOC_Loss=0 \
+                                    --encoder_alpha=0.5 --decoder_alpha=0.5 --encoder_gamma=0.5 --decoder_gamma=0.5 \
+                                    --encoder_lambda=0.5 --decoder_lambda=0.5 --beta=0.5 \
+                                    --lr=$lrs \
+                                    --main_model=$main_model_item \
+                                    --attention_dim=$attention_dim \
+                                    --num_attention_heads=$heads \
+                                    --seed=$seed \
+                                    --dataset=$dataset \
+                                    --window_size=$window_size_item \
+                                    --training_aerca=1 \
+                                    --epochs=1000 \
+                                    --early_stopping=0 \
+                                    --results_csv=RQ_1_swat.csv"
+
+                                eval $cmd
+                        done
+            
+        done
+    done
+}
+# --- Run experiments ---
+# 2. With AMOC
+run_experiment_RoGSTA_SWAT_iTransformer 1
 
 
 
@@ -270,7 +309,7 @@ run_experiment_Lotka_Volterra_Fedformer() {
 }
 # --- Run experiments ---
 # 2. With AMOC
-run_experiment_Lotka_Volterra_Fedformer 1
+#run_experiment_Lotka_Volterra_Fedformer 1
 
 
 
@@ -311,4 +350,4 @@ run_experiment_Lotka_Volterra_Fedformer() {
 }
 # --- Run experiments ---
 # 2. With AMOC
-run_experiment_Lotka_Volterra_Fedformer 1
+#run_experiment_Lotka_Volterra_Fedformer 1
