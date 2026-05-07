6b9a7c05ece9ef1baa7880e142959754597ad138
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2026-05-07 16:53:00 -0400

    (run for 8 -> 20 window)

--- RQ_1_aiops.sh
+++ RQ_1_aiops.sh
@@ -1,9 +1,9 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate RCAEval
 
-seeds=(1)
+seeds=(2 3)
 dataset="aiops"
-window_size=(6)
+window_size=(8 10 12 14 20)
 lrs=("1e-4")
 
 BETA_VAL=0.01
@@ -51,7 +51,7 @@ run_vlinear() {
                                     --epochs=200 \
                                     --early_stopping=0 \
                                     --combine_method="attention" \
-                                    --results_csv="RQ_1_aiops_allmodels_detailed.csv" \
+                                    --results_csv="RQ_1_aiops_30vars.csv" \
 
                                     --attention_dim="$attention_dim" \
                                     --num_attention_heads="$heads" \
@@ -107,7 +107,7 @@ run_GVAR() {
                                     --early_stopping=0 \
                                     --preprocessing_data=$preprocessing_data \
                                     --combine_method="attention" \
-                                    --results_csv="RQ_1_aiops_allmodels_detailed.csv" \
+                                    --results_csv="RQ_1_aiops_30vars.csv" \
 
                                     --attention_dim="$attention_dim" \
                                     --num_attention_heads="$heads" \
@@ -162,7 +162,7 @@ run_cLSTM() {
                                     --early_stopping=0 \
                                     --preprocessing_data=$preprocessing_data \
                                     --combine_method="attention" \
-                                    --results_csv="RQ_1_aiops_allmodels_detailed.csv" \
+                                    --results_csv="RQ_1_aiops_30vars.csv" \
 
                                     --attention_dim="$attention_dim" \
                                     --num_attention_heads="$heads" \
@@ -216,7 +216,7 @@ run_causalrca() {
                                     --early_stopping=0 \
                                     --preprocessing_data=$preprocessing_data \
                                     --combine_method="attention" \
-                                    --results_csv="RQ_1_aiops_allmodels_detailed.csv" \
+                                    --results_csv="RQ_1_aiops_30vars.csv" \
 
                                     --attention_dim="$attention_dim" \
                                     --num_attention_heads="$heads" \
@@ -236,30 +236,38 @@ run_causalrca() {
 
 
 
+
 run_experiment_baselines() {
     local preprocessing_data=$1
     local seed=$2
     local window_size_item=$3
 
-    local coeff_architecture=("torai")
+    local coeff_architecture=("torai" "baro" "rcd")
     local main_model="aerca_based" # Changed from array to string
 
     for arch in "${coeff_architecture[@]}"; do
         echo "Running: dataset=$dataset | seed=$seed | arch=$arch | window_size=$window_size_item"
-
+        # only run preprocessing for the first coeff_architecture to avoid redundant preprocessing
+        if [ $arch == "torai" ]; then
+            preprocessing_flag=$preprocessing_data
+        else
+            preprocessing_flag=0
+        fi
         cmd="python3 main.py \
             --seed=$seed \
             --dataset=$dataset \
             --coeff_architecture=$arch \
-            --preprocessing_data=$preprocessing_data \
+            --preprocessing_data=$preprocessing_flag \
             --window_size=$window_size_item \
             --main_model=$main_model \
             --training_aerca=0 \
-            --results_csv=RQ_1_aiops_allmodels_detailed.csv" # Removed the comment/backslash error here
+            --results_csv=RQ_1_aiops_30vars.csv" # Removed the comment/backslash error here
 
         eval $cmd
     done
 }
+
+
 
 
 
@@ -269,7 +277,7 @@ run_experiment_baselines() {
 
 for window_size_item in "${window_size[@]}"; do
     for seed in "${seeds[@]}"; do
-        if [ $seed -eq 1 ]; then
+        if [ $seed -eq 2 ]; then
             preprocessing_data=1
         else
             preprocessing_data=0
@@ -278,8 +286,8 @@ for window_size_item in "${window_size[@]}"; do
         if [ $preprocessing_data -eq 1 ]; then
             preprocessing_data=0
         fi
-        #run_GVAR $preprocessing_data $seed $window_size_item
+        run_GVAR $preprocessing_data $seed $window_size_item
         #run_experiment_baselines $preprocessing_data $seed $window_size_item
-        #run_cLSTM $preprocessing_data $seed $window_size_item
+        run_cLSTM $preprocessing_data $seed $window_size_item
     done
 done
