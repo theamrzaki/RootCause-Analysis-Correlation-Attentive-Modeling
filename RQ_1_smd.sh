f344e2149ec5b73d3fee0bede7505011b824fdbd
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2026-05-11 12:32:36 -0400

    (CUTs PLUS 8,10)

--- RQ_1_smd.sh
+++ RQ_1_smd.sh
@@ -390,7 +390,7 @@ run_CUTS_PLUS_aiops() {
 
 
 seeds=(2 3)
-window_size=(8 10 16 20)
+window_size=(12)
 
 for seed in "${seeds[@]}"; do
     for window_size_item in "${window_size[@]}"; do
@@ -399,4 +399,167 @@ for seed in "${seeds[@]}"; do
             preprocessing_data=0
     done
 done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-------------------gaia-----------------------
+
+
+
+
+
+
+
+
+
+dataset="gaia"
+
+run_CUTS_PLUS_gaia() {
+    local preprocessing_data=$1
+    local seed=$2
+    local window_size_item=$3
+
+    local arch="CUTS_PLUS"
+    local main_model="aerca_based"
+    local attention_dim=16
+    local heads=2
+    local outer_att_dim_val=16
+    local outer_heads_val=2
+
+                                echo "Running: dataset=$dataset | seed=$seed | window_size=$window_size_item | lr=$lrs | main_model=$main_model"
+
+
+                                cmd="python3 main.py \
+                                        --correlated_KL=0 --mean_std_recon_loss=0 --AMOC_Loss=0 \
+                                        --encoder_gamma=$GAMMA_VAL --decoder_gamma=$GAMMA_VAL \
+                                        --encoder_lambda=$LAMBDA_VAL --decoder_lambda=$LAMBDA_VAL --beta=$BETA_VAL \
+
+                                    --main_model=$main_model \
+                                    --coeff_architecture="$arch" \
+                                    --time_freq_representation="mag_phase" \
+
+                                    --preprocessing_data="$preprocessing_data" \
+
+                                    --lr="$lrs" \
+                                    --seed="$seed" \
+                                    --dataset="$dataset" \
+                                    --window_size="$window_size_item" \
+
+                                    --training_aerca=1 \
+                                    --epochs=200 \
+                                    --early_stopping=0 \
+                                    --combine_method="attention" \
+                                    --results_csv="RQ_1_gaia_allmodels_50vars.csv" \
+
+                                    --attention_dim="$attention_dim" \
+                                    --num_attention_heads="$heads" \
+                                    --outer_heads_num="$outer_heads_val" \
+                                    --outer_hidden_dim="$outer_att_dim_val" \
+
+                                       "
+
+                                eval $cmd
+                        
+            
+}
+
+
+seeds=(1 2 3)
+window_size=(8 10 12 14)
+
+for seed in "${seeds[@]}"; do
+    for window_size_item in "${window_size[@]}"; do
+            preprocessing_data=1
+        run_CUTS_PLUS_gaia $preprocessing_data $seed $window_size_item
+            preprocessing_data=0
+    done
+done
+
+
+
+
+
+
+
+#-------------------SMD-----------------------
+
+
+
+
+dataset="smd"
+
+run_CUTS_PLUS_SMD() {
+    local preprocessing_data=$1
+    local seed=$2
+    local window_size_item=$3
+
+    local arch="CUTS_PLUS"
+    local main_model="aerca_based"
+    local attention_dim=16
+    local heads=2
+    local outer_att_dim_val=16
+    local outer_heads_val=2
+
+                                echo "Running: dataset=$dataset | seed=$seed | window_size=$window_size_item | lr=$lrs | main_model=$main_model"
+
+
+                                cmd="python3 main.py \
+                                        --correlated_KL=0 --mean_std_recon_loss=0 --AMOC_Loss=0 \
+                                        --encoder_gamma=$GAMMA_VAL --decoder_gamma=$GAMMA_VAL \
+                                        --encoder_lambda=$LAMBDA_VAL --decoder_lambda=$LAMBDA_VAL --beta=$BETA_VAL \
+
+                                    --main_model=$main_model \
+                                    --coeff_architecture="$arch" \
+                                    --time_freq_representation="mag_phase" \
+
+                                    --preprocessing_data="$preprocessing_data" \
+
+                                    --lr="$lrs" \
+                                    --seed="$seed" \
+                                    --dataset="$dataset" \
+                                    --window_size="$window_size_item" \
+
+                                    --training_aerca=1 \
+                                    --epochs=200 \
+                                    --early_stopping=0 \
+                                    --combine_method="attention" \
+                                    --results_csv="RQ_1_smd_allmodels_8May.csv" \
+
+                                    --attention_dim="$attention_dim" \
+                                    --num_attention_heads="$heads" \
+                                    --outer_heads_num="$outer_heads_val" \
+                                    --outer_hidden_dim="$outer_att_dim_val" \
+
+                                       "
+
+                                eval $cmd
+                        
+            
+}
+
+
+seeds=(1 2 3)
+window_size=(4 6 10 20 30)
+
+for seed in "${seeds[@]}"; do
+    for window_size_item in "${window_size[@]}"; do
+            preprocessing_data=1
+        run_CUTS_PLUS_SMD $preprocessing_data $seed $window_size_item
+            preprocessing_data=0
+    done
+done
+
+
+
 
