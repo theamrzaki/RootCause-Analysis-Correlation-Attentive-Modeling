340e99dd8aebdafe80d9da57b6cbf19153a22e2f
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2026-07-31 11:27:31 -0400

    (gaia var exps)

--- datasets/gaia.py
+++ datasets/gaia.py
@@ -13,7 +13,9 @@ class GAIA:
     def __init__(self, options):
         self.options = options
         self.data_dir = options.get('data_dir')
+        self.data_dir_path_modified_with_window_var = False
         self.window_size = options.get('window_size')
+        self.num_vars = options.get('num_vars')
         #self.service_prefix = options.get('service_prefix') # Adjust based on your target service in GAIA
         self.service_prefixes =  ['dbservice1', 'authservice', 'cache']
         self.urls = {
@@ -374,11 +376,17 @@ class GAIA:
 
 
     def save_data(self):
-        if not os.path.exists(self.data_dir): os.makedirs(self.data_dir)
+        #if not os.path.exists(self.data_dir): os.makedirs(self.data_dir)
+        self.data_dir = os.path.join(self.data_dir, f"window_{self.window_size}_vars_{self.num_vars}")
+        os.makedirs(self.data_dir, exist_ok=True)
+        self.data_dir_path_modified_with_window_var = True
         for key in ['x_n_list', 'x_ab_list', 'label_list']:
             np.save(os.path.join(self.data_dir, f'{key}.npy'), self.data_dict[key])
 
     def load_data(self):
+        if not self.data_dir_path_modified_with_window_var:
+            self.data_dir = os.path.join(self.data_dir, f"window_{self.window_size}_vars_{self.num_vars}")
+            self.data_dir_path_modified_with_window_var = True
         self.data_dict['x_n_list'] = np.load(os.path.join(self.data_dir, 'x_n_list.npy'))
         self.data_dict['x_ab_list'] = np.load(os.path.join(self.data_dir, 'x_ab_list.npy'))
         self.data_dict['label_list'] = np.load(os.path.join(self.data_dir, 'label_list.npy'))
