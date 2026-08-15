cbd95032aa397f15b2bf74d3b8d63d8b48aa2157
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2026-08-15 08:32:58 -0400

    (windows)

--- ablations_search_windows.sh
+++ ablations_search_windows.sh
@@ -92,38 +92,38 @@ preprocessing_data=0
 batch_size=512
 hidden_layer_size=128
 epochs=50
-for seed in "${seeds[@]}"; do
-    for window_size_item in "${window_size[@]}"; do
-
-            beta=0.01
-            gamma=0.5
-            lambda=0.5
-
-            echo "================================================"
-            echo "window_size_item: $window_size_item"
-            echo "================================================"
-
-            run_deep_models \
-                $preprocessing_data \
-                $seed \
-                $window_size_item \
-                "vlinear" \
-                $latent_mode \
-                $context \
-                $pool \
-                $coeff_mode \
-                $predictor \
-                $temporal_mixer \
-                $beta \
-                $lambda \
-                $gamma \
-                $batch_size \
-                $hidden_layer_size \
-                $epochs
-
-        
-    done
-done
+#for seed in "${seeds[@]}"; do
+#    for window_size_item in "${window_size[@]}"; do
+#
+#            beta=0.01
+#            gamma=0.5
+#            lambda=0.5
+#
+#            echo "================================================"
+#            echo "window_size_item: $window_size_item"
+#            echo "================================================"
+#
+#            run_deep_models \
+#                $preprocessing_data \
+#                $seed \
+#                $window_size_item \
+#                "vlinear" \
+#                $latent_mode \
+#                $context \
+#                $pool \
+#                $coeff_mode \
+#                $predictor \
+#                $temporal_mixer \
+#                $beta \
+#                $lambda \
+#                $gamma \
+#                $batch_size \
+#                $hidden_layer_size \
+#                $epochs
+#
+#        
+#    done
+#done
 
 
 
@@ -179,3 +179,67 @@ epochs=200
 #        
 #    done
 #done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+dataset="BATADAL"
+results_csv="4_Ablations_batadal_windows.csv"
+
+seeds=(1 2 3)
+window_size=(4 8 16 24 32) #4 16)
+
+latent_mode="mul"
+context="linear_attn"
+pool="max"
+coeff_mode="symmetric"
+predictor="linear"
+temporal_mixer=1
+
+# Data has already been preprocessed.
+preprocessing_data=1
+batch_size=512
+hidden_layer_size=128
+epochs=200
+for seed in "${seeds[@]}"; do
+    for window_size_item in "${window_size[@]}"; do
+
+            beta=0.005
+            gamma=0.5
+            lambda=0.5
+
+            echo "================================================"
+            echo "window_size_item: $window_size_item"
+            echo "================================================"
+
+            run_deep_models \
+                $preprocessing_data \
+                $seed \
+                $window_size_item \
+                "vlinear" \
+                $latent_mode \
+                $context \
+                $pool \
+                $coeff_mode \
+                $predictor \
+                $temporal_mixer \
+                $beta \
+                $lambda \
+                $gamma \
+                $batch_size \
+                $hidden_layer_size \
+                $epochs
+
+        
+    done
+done
